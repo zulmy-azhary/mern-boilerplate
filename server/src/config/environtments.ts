111a24b6dccf14c4985959995ecf95ee081ab924
zoel-a <zulmyazhary32@gmail.com>
2024-02-01 03:47:26 +0800

    refactor: add jwt secret's env

--- server/src/config/environtments.ts
+++ server/src/config/environtments.ts
@@ -1,9 +1,8 @@
 import "dotenv/config";
 
-const config = {
+export const config = {
   db: process.env.DB,
   port: process.env.PORT,
-  clientBaseUrl: process.env.CLIENT_BASE_URL
+  clientBaseUrl: process.env.CLIENT_BASE_URL,
+  jwtSecret: process.env.JWT_SECRET
 };
-
-export default config;
