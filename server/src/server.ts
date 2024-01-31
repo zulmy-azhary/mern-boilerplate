3d813087be7bc80c9e3d3c67704364bbc0377ead
zoel-a <zulmyazhary32@gmail.com>
2024-02-01 03:58:55 +0800

    feat: add mongodb connection, deserializing token, add root route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,17 +2,23 @@
 import dotenv from "dotenv";
 import express, { type NextFunction, type Request, type Response } from "express";
 import mongoose from "mongoose";
-import config from "./config/environtments";
+import { config } from "./config/environtments";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
-import { logger } from "./utils/logger";
+import { logger } from "./libs/logger";
 import { routes } from "./routes";
+import { deserializedToken } from "./middlewares/auth.middleware";
 
 dotenv.config();
 
+// MongoDB connection
 mongoose.set("strictQuery", true);
+mongoose
+  .connect(config.db as string)
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch(err => logger.error(err));
 
 const app = express();
 const port = config.port || 5000;
@@ -42,8 +48,20 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
+app.use(deserializedToken);
+
 // All Routes
 routes(app);
+
+app.use("/", (req: Request, res: Response) => {
+  try {
+    logger.info("HEALTH -> GET_HEALTH = Health OK! Server is running");
+    return res.status(200).send({ success: true, code: 200, message: "Health OK! Server is running" });
+  } catch (err) {
+    logger.error(`HEALTH -> GET_HEALTH = ${(err as Error).message}`);
+    return res.status(500).send({ success: false, error: { code: 500, message: (err as Error).message } });
+  }
+});
 
 app.listen(port, () => {
   logger.info(`Server listening on port ${port}, url: http://localhost:${port}`);
