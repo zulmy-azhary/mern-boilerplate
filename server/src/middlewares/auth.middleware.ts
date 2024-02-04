052ad6a275d578984bd27a9877982f2d849e6844
zoel-a <zulmyazhary32@gmail.com>
2024-02-04 23:34:14 +0800

    refactor: update deserializedToken and requireUser middleware

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -4,35 +4,48 @@ import { logger } from "../libs/logger";
 
 export const requireUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    // Check if res.locals.user passed from deserializedToken
     const user = res.locals.user;
-
+    // If token not valid, then return 403 forbidden
     if (!user) {
-      logger.error("AUTH -> REQUIRE USER = Access Denied.");
-      return res.status(403).send({ status: false, statusCode: 403, message: "Access Denied." });
+      res.clearCookie("accessToken", { httpOnly: true });
+      logger.error("AUTH -> REQUIRE_USER = Access Denied.");
+      return res.status(403).send({ success: false, code: 403, message: "Access Denied." });
     }
 
+    // If token is valid, then handle the next middleware or controller
     next();
   } catch (err) {
-    return res.status(500).send({ status: false, statusCode: 500, message: (err as Error).message });
+    return res.status(500).send({ success: false, code: 500, message: (err as Error).message });
   }
 };
 
 // Token
 export const deserializedToken = async (req: Request, res: Response, next: NextFunction) => {
-  const accessToken = req.headers.authorization?.replace(/^Bearer\s/, "");
+  // Get accessToken from httpOnly cookies
+  const accessToken: string | undefined = req.cookies.accessToken;
   if (!accessToken) {
     return next();
   }
 
   const token = verifyToken(accessToken);
+  if (token.expired) {
+    return next();
+  }
+
+  // If decoded exist, then set response locals user to use to the next middleware
+  // For example: (..., deserializedToken, requireUser, ...)
   if (token.decoded) {
     res.locals.user = token.decoded;
     return next();
   }
 
-  if (token.expired) {
-    next();
-  }
-
   next();
+};
+
+export const generateNewAccessToken = async (req: Request, res: Response, next: NextFunction) => {
+  const refreshToken: string | undefined = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return next();
+  }
 };
