4f7f5b8d64e2c35b48e6f91baa1d24507e78dbde
zoel-a <zulmyazhary32@gmail.com>
2024-02-26 22:10:12 +0800

    feat: create user account's schema

---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+export const accountSchema = z
+  .object({
+    name: z.optional(
+      z
+        .string()
+        .min(1, "Name is required.")
+        .min(4, "Name must be at least 4 characters.")
+        .max(24, "Maximum length of Name is 24 characters.")
+    ),
+    email: z.optional(z.string().email("Invalid Email Address.")),
+    password: z.optional(z.string().min(6, "Password must be at least 6 characters.")),
+    newPassword: z.optional(z.string().min(6, "New Password must be at least 6 characters.")),
+    role: z.enum(["Admin", "User"])
+  })
+  .refine(
+    data => {
+      if (!data.password && data.newPassword) return false;
+      return true;
+    },
+    {
+      message: "Password is required.",
+      path: ["password"]
+    }
+  )
+  .refine(
+    data => {
+      if (data.password && !data.newPassword) return false;
+      return true;
+    },
+    {
+      message: "New Password is required.",
+      path: ["newPassword"]
+    }
+  );
