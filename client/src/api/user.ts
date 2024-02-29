48dabc51df2564babb18b1429a4ae7a5008a4155
zoel-a <zulmyazhary32@gmail.com>
2024-02-29 12:59:29 +0800

    feat: add update account api request

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,7 +1,15 @@
 import { axiosWithCredentials } from "@/api/axios";
+import type { accountSchema } from "@/schemas/user.schema";
 import type { Response } from "@/types/api";
+import type { MongoDocument } from "@/types/common";
 import type { UserInfos } from "@/types/user";
+import type { z } from "zod";
 
 export const me = async () => {
   return await axiosWithCredentials.get<Response<UserInfos>>("/user/me");
+};
+
+export const updateAccount = async (payload: MongoDocument<z.infer<typeof accountSchema>>) => {
+  const { _id, ...userInfos } = payload;
+  return await axiosWithCredentials.post<Response>(`/user/update-account/${_id}`, userInfos);
 };
