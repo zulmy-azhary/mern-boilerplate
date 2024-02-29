11bb09fb7e8a7adff2c438b4c1cb1005e2a29f70
zoel-a <zulmyazhary32@gmail.com>
2024-02-29 13:08:31 +0800

    feat: createAccessToken jwt lib

--- server/src/libs/jwt.ts
+++ server/src/libs/jwt.ts
@@ -1,5 +1,6 @@
 import { type Secret, type SignOptions, type JwtPayload, sign, verify } from "jsonwebtoken";
 import { config } from "../config/environtments";
+import type { Response } from "express";
 
 export const generateAccessToken = (payload: Record<string, unknown>, options?: SignOptions) => {
   return sign(payload, config.jwtSecret as Secret, {
@@ -23,4 +24,18 @@ export const verifyToken = <T = string | JwtPayload>(token: string) => {
       decoded: null
     };
   }
+};
+
+// Create and send accessToken to httpOnly cookie
+export const createAccessToken = (res: Response, payload: Record<string, unknown>) => {
+  const accessToken = generateAccessToken(payload, { expiresIn: "1d" }); // Expires 1d
+
+  // Set httpOnly cookie
+  const expires = new Date(new Date().setDate(new Date().getDate() + 1)); // Expires 1d
+  return res.cookie("accessToken", accessToken, {
+    expires,
+    httpOnly: true,
+    sameSite: "strict", // To prevent CSRF attacks
+    secure: process.env.NODE_ENV === "production" // Set secure true in production
+  });
 };
