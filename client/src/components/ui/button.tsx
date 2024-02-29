75ee6925d1d8033398d609a16b0cf973370af592
zoel-a <zulmyazhary32@gmail.com>
2024-02-29 12:03:51 +0800

    style: add primary variant

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -12,6 +12,7 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         destructive: "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline: "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        primary: "bg-blue-500 shadow-sm text-primary-foreground hover:bg-blue-500/90",
         secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline"
