527390a9932c2080022e61d60d053ffffce92337
zoel-a <zulmyazhary32@gmail.com>
2024-02-29 12:08:41 +0800

    refactor: update email's rule

--- client/src/schemas/user.schema.ts
+++ client/src/schemas/user.schema.ts
@@ -9,7 +9,7 @@ export const accountSchema = z
         .min(4, "Name must be at least 4 characters.")
         .max(24, "Maximum length of Name is 24 characters.")
     ),
-    email: z.optional(z.string().email("Invalid Email Address.")),
+    email: z.string().email("Invalid Email Address."),
     password: z.optional(z.string().min(6, "Password must be at least 6 characters.")),
     newPassword: z.optional(z.string().min(6, "New Password must be at least 6 characters.")),
     role: z.enum(["Admin", "User"])
